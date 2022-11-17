91769c109020b665a6719a6f715100389209085a
Mahmudur Rahman <nahidacm@gmail.com>
2022-11-17 17:07:25 +0600

    Content tiles are perfetly responsive and looks good

--- src/App.js
+++ src/App.js
@@ -96,38 +96,68 @@ function App() {
           {/* Suggested section starts */}
           <div>Suggested</div>
           <div className="grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-1 gap-4">
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+           
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/apples.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/black-berry.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/fruit-bowl.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+           
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/apples.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/black-berry.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/fruit-bowl.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+           
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/apples.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
-            <div className="border-4 border-slate-200">
-              <img src="/images/file-preview.jpg" alt="File Preview" />
-              <div>The file name</div>
+
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/black-berry.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
             </div>
+            <div className="h-64 border-2 rounded-lg border-slate-200">
+              <div className="h-52 bg-slate-100 rounded-t-lg overflow-hidden">
+                <img className="p-1 rounded-lg shadow-lg w-full" src="/images/fruit-bowl.jpg" alt="File Preview" />
+              </div>
+              <div className="text-gray-500 font-bold p-2 border-t-2 border-t-slate-200">The file name</div>
+            </div>
+
+          
             
           </div>
           {/* Suggested section ends */}
