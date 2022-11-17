cdf0b37042833e18b0150125033f529fcb3cc7e4
Mahmudur Rahman <nahidacm@gmail.com>
2022-11-17 12:50:30 +0600

    File server UI with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      {/* Top Row */}
+      <div className="flex h-16 border-b-2 border-b-slate-200 py-2 mb-5">
+        <div className="flex">
+          <img className="" src="/images/logo.svg" alt="Logo" />
+        </div>
+        <input
+          type="text"
+          className="flex-grow rounded mx-16 my-2 px-2 border border-x-gray-100 focus:outline-0"
+        />
+        <div className="flex">
+          <img
+            className="rounded-full"
+            src="/images/alexander-hipp.jpg"
+            alt="Hi"
+          />
+        </div>
+      </div>
+      {/* End Top Row */}
+
+      <div className="flex">
+        {/* Left column starts */}
+        <div className="flex flex-col pr-10">
+          <button className="flex rounded-full border-4 py-2 px-6">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+              />
+            </svg>
+            Upload
+          </button>
+
+          <div className="mt-8">
+            <div className="flex py-2 hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+              </svg>
+              My Drive
+            </div>
+            <div className="flex py-2 hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+              </svg>
+              Shared With Me
+            </div>
+            <div className="flex py-2 hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              Recent
+            </div>
+            <div className="flex py-2 hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+              </svg>
+              Starred
+            </div>
+            <div className="flex py-2 hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+              Trash
+            </div>
+          </div>
+          
+        </div>
+        {/* Left column ends */}
+
+        {/* Right content starts */}
+        <div className="flex-grow">
+          {/* Right content header starts */}
+          <div className="flex border-b-2 pb-5">
+            <div>My Drive</div>
+            <div className="flex flex-grow justify-end">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-3">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+              </svg>
+            </div>
+          </div>
+          {/* Right content header ends */}
+
+          {/* Suggested section starts */}
+          <div>Suggested</div>
+          <div className="grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-1 gap-4">
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            <div className="border-4 border-slate-200">
+              <img src="/images/file-preview.jpg" alt="File Preview" />
+              <div>The file name</div>
+            </div>
+            
+          </div>
+          {/* Suggested section ends */}
+
+        </div>
+      </div>
     </div>
   );
 }
