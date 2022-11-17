96401dea288df32a9391da9a906cf67158ec8a1b
Mahmudur Rahman <nahidacm@gmail.com>
2022-11-17 17:20:35 +0600

    Left menu style fix

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
               </svg>
-              Shared With Me
+              <div className="whitespace-nowrap">Shared With Me</div>
             </div>
             <div className="flex py-2 hover:cursor-pointer">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
@@ -94,7 +94,7 @@ function App() {
           {/* Right content header ends */}
 
           {/* Suggested section starts */}
-          <div>Suggested</div>
+          <div className="my-5 text-xl text-gray-500">Suggested</div>
           <div className="grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-1 gap-4">
            
             <div className="h-64 border-2 rounded-lg border-slate-200">
